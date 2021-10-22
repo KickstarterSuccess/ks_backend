535d9befeefec708aa44b93595945fabdda18e42
RylandGomez <rylandalexandergomez@gmail.com>
2021-10-22 12:47:43 -0500

    Relocated model.pkl, updated app.py for full functionality

--- ks_flask/app.py
+++ ks_flask/app.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 from flask_cors import CORS, cross_origin
 from flask import Flask, request, jsonify
 from .preprocessing import get_dur, get_monthyear, predict_to_string
@@ -17,6 +18,8 @@ def create_app():
     def main():
         return "Welcome to the server!"
 
+
+
     @app.route("/predict", methods=["GET", "POST"])
     @cross_origin()
     def predict():
@@ -28,42 +31,39 @@ def create_app():
         # Retrieve data from front end
         data = request.get_json(force=True)
 
-        # # Run feature engineering functions
-        # data['duration'] = get_dur(data['date'], data['deadline'])
-        # data['month'], data['year'] = get_monthyear(data['date'])
-        data['duration'] = 'test1'
-        data['month'], data['year'] = 'test2', 'test3'
+        # Run feature engineering functions
+        data['duration'] = get_dur(data['date'], data['deadline'])
+        data['month'], data['year'] = get_monthyear(data['date'])
+        # data['duration'] = 'test1'
+        # data['month'], data['year'] = 'test2', 'test3'
+        
         # Define desired variables in X_pred order
         X_vars = ['goal','month','year','duration','country','currency','category']
 
         # Create empty list for populating X_pred
-        X_pred = []
+        X_pred_list = []
 
         # Iterate over X_vars to populate X_pred with key value pairs
         for x in X_vars:
-            X_pred.append(data[x])
+            X_pred_list.append(data[x])
 
-        # # Load locally stored pickled model
-        # model = pickle.load(open('model','rb'))
+        # Format list into 2D array for prediction
+        X_pred = np.array(X_pred_list)
+        X_pred = X_pred.reshape(1,-1)
 
-        # # Create prediction from model
-        # prediction = model.predict(X_pred)
+        # Load locally stored pickled model
+        model = pickle.load(open('ks_flask\model.pkl','rb'))
+        
+        # Create prediction from model
+        prediction = model.predict(X_pred)
 
-        # # Covert array to string response
-        # prediction = predict_to_string(prediction)
+        # Covert array to string response
+        prediction = predict_to_string(prediction)
 
-        # # JSONify the prediction
-        # prediction = jsonify({'prediction': prediction})
-        # # Return prediction (may need reformatting)
-        # return prediction
-        test = {'prediction':'SUCCESS!!!'}
-        return jsonify(test)
+        # JSONify the prediction
+        prediction = jsonify({'prediction': prediction})
 
-
-    @app.route("/test", methods=["GET", "POST"])
-    @cross_origin()
-    def test():
-        dummydata = {'name':1, 'country':2}
-        return jsonify(dummydata)
+        # Return prediction 
+        return prediction
 
     return app
