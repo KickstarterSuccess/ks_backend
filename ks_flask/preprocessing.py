bcbcdc1d836a735737f07c9d07fec34ab647b161
RylandGomez <rylandalexandergomez@gmail.com>
2021-10-22 12:54:51 -0500

    Fixed wrong methods for Timestamp

--- ks_flask/preprocessing.py
+++ ks_flask/preprocessing.py
@@ -16,8 +16,8 @@ def get_monthyear(date):
     Pull month and year from datetime object
     '''
     date_dt = pd.to_datetime(date)
-    month = date_dt.dt.month
-    year = date_dt.dt.year
+    month = date_dt.month
+    year = date_dt.year
 
     return month, year
 
@@ -26,9 +26,9 @@ def predict_to_string(array):
     Convert model prediction to a string value
     '''
 
-    if array[0][0] == 0:
+    if array[0] == 0:
         return 'The kickstarter is not predicted to succeed.'
-    if array[0][0] == 1:
+    if array[0] == 1:
         return 'The kickstarter is predicted to succeed!'
     else:
         return 'ERROR: predict_to_string function not properly formatted.'
